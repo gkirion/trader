112d59d1d5beafb8d20e5928efa397d5ce3f91b2
gkirion <gkirion@gmail.com>
2021-10-24 21:21:59 +0300

    Fixed local min formula

--- analyzer.py
+++ analyzer.py
@@ -5,7 +5,7 @@ def is_local_max(point, threshold):
     return False
 
 def is_local_min(point, threshold):
-    if point['left'] - point['mid'] > point['mid'] * threshold:
-        if point['right'] - point['mid'] > point['mid'] * threshold:
+    if point['mid'] - point['left'] < -point['left'] * threshold:
+        if point['mid'] - point['right'] < -point['right'] * threshold:
             return True
     return False
